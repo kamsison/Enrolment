3983ae848b6237525d3ffb0278e3ee475d22770e
kharl <kharlmostwanted@gmail.com>
2016-09-23 15:23:14 -0700

    p

--- routes/payments/payment.php
+++ routes/payments/payment.php
@@ -5,13 +5,13 @@
  * Date: 22/09/2016
  * Time: 4:28 PM
  */
-$app->get('/payment', function() use ($app) {
+$app->get('/payment/new', $isAuthenticated, function() use ($app) {
     $paymentModes = \um\models\PaymentMode::all();
     $periods = \um\models\Period::all();
     $app->render('payments/index.twig', compact('paymentModes', 'periods'));
 })->name('getPayment');
 
-$app->post('/payment', function() use ($app) {
+$app->post('/payment', $isAuthenticated, function() use ($app) {
     $payment = new \um\models\Payment();
     $payment->personal_info_id = $app->request->post('student_id');
     $payment->amount = $app->request->post('amount');
@@ -24,10 +24,21 @@ $app->post('/payment', function() use ($app) {
     //$app->render('payments/history.twig/', compact('payment'));
 })->name('postPayment');
 
-$app->get('/payment/history/:studentId', function($studentId) use ($app) {
+$app->get('/payment/history/:studentId', $isAuthenticated, function($studentId) use ($app) {
+    //var_dump($payments);
     $payments = \um\models\Payment::join('payment_modes', function($join) {
         $join->on('payment_modes.id', '=', 'payments.payment_mode_id');
+    })->join('periods', function($join){
+        $join->on('periods.id', '=', 'payments.period_id');
     })->where('personal_info_id', '=', $studentId)->orderBy('created_at', 'desc')->get();
-    //var_dump($payments);
     $app->render('payments/history.twig', compact('payments'));
+})->name('getPayment');
+
+$app->get('/payment/transactions', $isAuthenticated, function() use ($app) {
+    $payments = \um\models\Payment::join('payment_modes', function($join) {
+        $join->on('payment_modes.id', '=', 'payments.payment_mode_id');
+    })->join('periods', function($join){
+        $join->on('periods.id', '=', 'payments.period_id');
+    })->orderBy('created_at', 'desc')->get();
+    $app->render('payments/transactions.twig', compact('payments'));
 })->name('getPayment');
